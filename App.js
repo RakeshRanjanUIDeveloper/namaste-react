2cbbb9d60fc92d8814748f07791738159ea53639
Rakesh Ranjan <rakesh.j.ranjan@accenture.com>
2025-04-10 13:53:58 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,21 @@
+
+
+// const heading = React.createElement("h1", {
+//     id:'heading',
+//     abc:'xyz'
+// }, "Hello World from React");
+
+const parent = React.createElement('div', {id:'parent'}, 
+    [
+        React.createElement('div', {id:'child'},[
+            React.createElement('h1', {}, 'I am h1 tag'),
+            React.createElement('h1', {}, 'I am another h1 tag'),
+        ]),
+        React.createElement('div', {id:'child2'},[
+            React.createElement('h1', {}, 'I am h1 tag'),
+            React.createElement('h1', {}, 'I am another h1 tag'),
+        ])
+    ]
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent)
