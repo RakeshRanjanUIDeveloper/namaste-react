3fdb94ebf234f02126d483c6e8743a0c27efd275
Rakesh Ranjan <rakesh.j.ranjan@accenture.com>
2025-04-11 22:33:39 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-// const heading = React.createElement("h1", {
-//     id:'heading',
-//     abc:'xyz'
-// }, "Hello World from React");
 
-const parent = React.createElement('div', {id:'parent'}, 
-    [
-        React.createElement('div', {id:'child'},[
-            React.createElement('h1', {}, 'I am h1 tag'),
-            React.createElement('h1', {}, 'I am another h1 tag'),
-        ]),
-        React.createElement('div', {id:'child2'},[
-            React.createElement('h1', {}, 'I am h1 tag'),
-            React.createElement('h1', {}, 'I am another h1 tag'),
-        ])
-    ]
-)
+const HeadingComponent = () =>{
+    return <h1>Namaste React Functional Component</h1>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
+root.render(<HeadingComponent />)
